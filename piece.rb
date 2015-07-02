4b2fe88af293633d5004802b5b7341095590f819
App Academy Student <student@appacademy.io>
2015-07-02 11:06:30 -0700

    Add move directions method, remove directions ivar

--- piece.rb
+++ piece.rb
@@ -5,23 +5,32 @@ class Piece
     @pos = pos
     @board = board
     @king = false
-    @dir = [-1, 1]
   end
 
   def make_king
     @king = true
   end
 
+  def move_diffs
+    directions = []
+    if @king
+      directions += [[-1, -1], [-1, 1],[1, -1], [1, 1]]
+    else
+      @color == :R ? directions += [[-1, -1], [-1, 1]] : directions += [[1, -1], [1, 1]]
+    end
+
+  end
+
   def perform_slide
-    @pos = [@pos.first + dir.first], [@pos.last + dir.last]
   end
 
   def perform_jump
-    @pos =
   end
 
   def moves
-    i
+    valid_moves = []
+    @dir.each do |dir|
+
   end
 
   def perform_moves(list_of_moves)
@@ -30,4 +39,14 @@ class Piece
 end
 
 class EmptyPiece < Piece
+
+  def initialize
+  end
+
+  def perform_moves
+  end
+
+  def perform_jump
+  end
+
 end
