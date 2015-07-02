ae9d4c04c9bb4c95b289e2c6373d2008bc805108
App Academy Student <student@appacademy.io>
2015-07-02 10:56:31 -0700

    Add movement directions and move structure

--- board.rb
+++ board.rb
@@ -1,7 +1,13 @@
+require "colorize"
+require_relative "piece"
+
 class Board
 
   def initialize
-    @grid = Array.new(8) { Array.new(8) { [] } }
+    @grid = Array.new(8) { Array.new(8) { [EmptyPiece.new] } }
+  end
+
+  def render
   end
 
 end
--- piece.rb
+++ piece.rb
@@ -5,16 +5,28 @@ class Piece
     @pos = pos
     @board = board
     @king = false
+    @dir = @color == :R ? [[-1, -1], [-1, 1]] : [[1, -1], [1, 1]]
   end
 
   def make_king
     @king = true
   end
 
-  def move
+  def perform_slide
+    @pos = [@pos.first + dir.first], [@pos.last + dir.last]
   end
-  
+
+  def perform_jump
+  end
+
+  def moves
+    i
+  end
+
   def perform_moves(list_of_moves)
   end
 
+end
+
+class EmptyPiece < Piece
 end
